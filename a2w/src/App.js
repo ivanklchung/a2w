bdf982902f5db5629497bef83aaa221e31d152f1
Ivan Chung <103023514+ivanklchung@users.noreply.github.com>
2024-01-30 17:53:11 -0800

    Basic Setup

---
+++ a2w/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My Album Covers Website</h1>
+        <p>Welcome to my website where I turn music album covers into interactive pages!</p>
+      </header>
+      {/* Add your custom components and content here */}
+    </div>
+  );
+}
+
+export default App;
